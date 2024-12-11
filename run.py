aa6fd6aae0968c602b23e732ee93d91e3bd109eb
Yaroslav <340999@gmail.com>
2024-12-11 22:11:35 +0300

    task completed

---
+++ run.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+
+dates = {
+    "The Moscow Times": "Wednesday, October 2, 2002",
+    "The Guardian": "Friday, 11.10.13",
+    "Daily News": "Thursday, 18 August 1977",
+  }
+
+formats = {
+    "The Moscow Times": "%A, %B %d, %Y",
+    "The Guardian": "%A, %d.%m.%y",
+    "Daily News": "%A, %d %B %Y",
+  }
+
+
+
+def main() -> None:
+  for newspaper, date in dates.items():
+    print(f"{datetime.strptime(date, formats[newspaper])}")
+
+
+
+main()
